29fbd0c414e54d2d530b86f650d5bf54549a463e
Andy Spohn <Andy@AndySpohn.com>
2013-01-03 12:06:28 -0500

    Change rpmbuild command to just look for *.spec file instead of a specific file name

--- SOURCES/jenkins-rpm-build/usr/local/bin/jenkins-build-rpm.sh
+++ SOURCES/jenkins-rpm-build/usr/local/bin/jenkins-build-rpm.sh
@@ -2,12 +2,8 @@
 #
 # jenkins-build-rpm.sh - Top level script that runs all the commands needed to build an rpm task in Jenkins
 #
-# If all the conventions have been followed this script can be used to keep the Execute shell command window short
+# If we follow a simple naming convention we can keep the Execute shell command window short
 #   - Jenkins job has the same name as the RPM
-#   - RPM spec file has the same name as the RPM
-#
-# With these two naming conventions we can now use the Jenkins $JOB_NAME environment var and not have to update
-# scripts for each new job. Being able to just copy one task and change the name makes it much easier to set up quickly
 #
 
 # The undefined env vars are available within the jenkins runtime
@@ -23,7 +19,8 @@ if [ -d $PROJECT_WORKSPACE/SRPMS ] ; then
     rm -fr $PROJECT_WORKSPACE/SRPMS
 fi
 
+# Build the rpm passing in several args from the Jenkins environment
 rpmbuild \
     --define "_topdir $PROJECT_WORKSPACE" \
     --define "release `date +%Y%m%d%H%M%S`" \
-    -ba SPECS/$JOB_NAME.spec
+    -ba SPECS/*.spec
